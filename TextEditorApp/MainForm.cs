af6b766077f8e341e8b9987d791713c138a9ba99
Vlada <aladka03@gmail.com>
2022-03-07 19:48:21 +0300

    Created app and library projects

---
+++ TextEditorApp/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditorApp
+{
+    public partial class MainForm : Form
+    {
+        private Label header = new Label(),
+            chooseFileLabel = new Label();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            header.Name = "Header";
+            header.Text = "Text Editor";
+            header.Font = new Font(Font.FontFamily, 13);
+            header.Location = new Point(130, 15);
+            Controls.Add(header);
+
+            chooseFileLabel.Name = "ChoseFileLabel";
+            chooseFileLabel.Text = "Chose file";
+            chooseFileLabel.Font = new Font(Font.FontFamily, 11);
+            chooseFileLabel.Location = new Point(50, 40);
+            Controls.Add(chooseFileLabel);
+        }
+    }
+}
