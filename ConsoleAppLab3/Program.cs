878c92ca969adc0c89dd6ba67ed1a2daffc5d0be
Vlada <aladka03@gmail.com>
2022-03-07 15:50:09 +0300

    Implemented necessary functionality

---
+++ ConsoleAppLab3/Program.cs
@@ -0,0 +1,49 @@
+﻿using PersonLibrary;
+using System.Reflection;
+
+Type myType = typeof(Person);
+
+Console.WriteLine($" Type: {myType}");
+
+// Reading values of private fields
+Person person = new Person("Tom", 32, "skunk");
+
+FieldInfo? fieldInt = typeof(Person).GetField("_age", BindingFlags.Instance | BindingFlags.NonPublic);
+FieldInfo? fieldStr = typeof(Person).GetField("_nickname", BindingFlags.Instance | BindingFlags.NonPublic);
+
+int? age = (int?)fieldInt?.GetValue(person);
+string? nickname = (string?)fieldStr?.GetValue(person);
+
+Console.WriteLine("\n Values of private fields:");
+Console.WriteLine($" - Age: {age}");
+Console.WriteLine($" - Nickname: {nickname}");
+
+
+// Private methods
+Console.WriteLine("\n All private methods:");
+
+foreach (MemberInfo method in myType.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+{
+	Console.WriteLine($" - {method}");
+}
+
+
+// Public methods
+Console.WriteLine("\n All public methods:");
+
+foreach (MemberInfo member in myType.GetMethods())
+{
+	Console.WriteLine($" - {member.Name}");
+}
+
+
+// Private fields
+Console.WriteLine("\n All private fields:");
+
+foreach (MemberInfo field in myType.GetFields(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+{
+	Console.WriteLine($" - {field}");
+}
+	
+
+
---
+++ PersonLibrary/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonLibrary
+{
+    public class Person
+    {
+        public string Name;
+        private int _age;
+        private string _nickname;
+
+        public Person(string name, int age, string nickname)
+        {
+            Name = name;
+            _age = age;
+            _nickname = nickname;
+        }
+
+        public static string SayHello() => "Hello";
+
+        public string SaySometingStupid(string somethingStupid) => $"Hey! I'm {Name} and {somethingStupid}!";
+
+        private void IncreaseAge() => _age++;
+
+        private static int GetCountOfTeef() => 32;
+    }
+}
