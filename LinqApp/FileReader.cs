c21898e3cc4a3732ef64a7ee6bf0f8b11a261fc9
Vlada <aladka03@gmail.com>
2022-04-18 13:33:35 +0300

    sub commit

---
+++ LinqApp/FileReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApp
+{
+    internal static class FileReader
+    {
+        public static List<string> DataList { get; private set; }
+
+        public static void Read(string path)
+        {
+            DataList = new List<string>();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    DataList.Add(line);
+                }
+            }
+        }
+    }
+}
---
+++ LinqApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using LinqApp;
+
+try
+{
+    FileReader.Read(@"..\file1.txt");
+
+    Console.WriteLine("Data from file:");
+    foreach (string line in FileReader.DataList)
+        Console.WriteLine(line);
+
+    Console.WriteLine("\nResult:");
+
+    var result = from line in FileReader.DataList
+                 from digit in line.Split(' ').OfType<double>()
+                 select digit;
+
+    foreach (double r in result)
+        Console.WriteLine(r);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+            
